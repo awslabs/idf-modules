3a815eaf13e2ef9431804bf1675b6487580f8fb6
srinivasreddych <srinivasreddych@outlook.com>
2023-07-21 17:44:49 -0500

    testing

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+set -euo pipefail
+
+# CDK_VERSION=${1}
+
+# #relative to modules repo
+# array=(
+#     "networking/basic-cdk"
+#     "storage/buckets"
+# )
+
+
+# for i in "${array[@]}"; do   # The quotes are necessary here
+#     echo "$i"
+#     npm install -g aws-cdk@${CDK_VERSION}
+#     pip install -r requirements-dev.txt
+#     pip install -r modules/$i/requirements.txt
+#     pytest modules/$i --cov-config=modules/$i/coverage.ini --cov --cov-report=html:$CODEBUILD_SRC_DIR/reports/$i/html_dir --cov-report=xml:$CODEBUILD_SRC_DIR/reports/$i/coverage.xml
+# done
+
+####
+while getopts c:m: flag
+do
+    case "${flag}" in
+        c) CDK_VERSION=${OPTARG};;
+        m) MODULE_PATH=${OPTARG};;
+    esac
+done
+
+echo "CDK Version: $CDK_VERSION";
+echo "Modules Path: $MODULE_PATH";
+
+npm install -g aws-cdk@${CDK_VERSION}
+pip install -r requirements-dev.txt
+pip install -r $CODEBUILD_SRC_DIR/modules/$MODULE_PATH/requirements.txt
+pytest $CODEBUILD_SRC_DIR/modules/$MODULE_PATH --cov-config=modules/$MODULE_PATH/coverage.ini --cov --cov-report=html:$CODEBUILD_SRC_DIR/reports/$MODULE_PATH/html_dir --cov-report=xml:$CODEBUILD_SRC_DIR/reports/$MODULE_PATH/coverage.xml
+
