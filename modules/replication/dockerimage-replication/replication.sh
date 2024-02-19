d9f89404bfc3c3d01921dd6788f718984f2aca9d
a13zen <85226895+a13zen@users.noreply.github.com>
2024-02-19 16:00:18 +0100

    Avoid race conditions

--- modules/replication/dockerimage-replication/replication.sh
+++ modules/replication/dockerimage-replication/replication.sh
@@ -14,17 +14,17 @@ create() {
         IMAGE_ACCOUNT_ID=$(echo $image_name | awk -F '/' '{print $1}' | awk -F '.' '{print $1}')
         aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $IMAGE_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
     fi
-    echo Pulling the $image
 
     TARGET_REPOSITORY_NAME=${AWS_CODESEEDER_NAME}-${image_name}
     IMAGE_META="$( aws ecr batch-get-image --repository-name=$TARGET_REPOSITORY_NAME --image-ids=imageTag=$image_tag --query 'images[].imageId.imageTag' --output text )" || true
     if [[ $IMAGE_META == $image_tag ]]; then
-    echo "$IMAGE_META found skipping build"
+    echo "$IMAGE_META found in $TARGET_REPOSITORY_NAME skipping build"
     else
     echo "$TARGET_REPOSITORY_NAME:$image_tag not found, fetching"
+    echo Pulling $image
     docker pull $image
     # Setting connection with AWS ECR
-    aws ecr describe-repositories --repository-names $TARGET_REPOSITORY_NAME || aws ecr create-repository --repository-name $TARGET_REPOSITORY_NAME --image-scanning-configuration scanOnPush=true
+    aws ecr describe-repositories --repository-names $TARGET_REPOSITORY_NAME || $(aws ecr create-repository --repository-name $TARGET_REPOSITORY_NAME --image-scanning-configuration scanOnPush=true && sleep 10)
     aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
     # Tagging and pushing Docker images according to https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-pull-ecr-image.html
     docker tag $image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$TARGET_REPOSITORY_NAME:${image_tag}
