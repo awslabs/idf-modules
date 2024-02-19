6e69bee44439fb3fdab3ef4337ad48849ad24990
a13zen <85226895+a13zen@users.noreply.github.com>
2024-02-19 16:31:30 +0100

    sleep after create to avoid race condition

--- modules/replication/dockerimage-replication/replication.sh
+++ modules/replication/dockerimage-replication/replication.sh
@@ -24,7 +24,8 @@ create() {
     echo Pulling $image
     docker pull $image
     # Setting connection with AWS ECR
-    aws ecr describe-repositories --repository-names $TARGET_REPOSITORY_NAME || $(aws ecr create-repository --repository-name $TARGET_REPOSITORY_NAME --image-scanning-configuration scanOnPush=true && sleep 10)
+    aws ecr describe-repositories --repository-names $TARGET_REPOSITORY_NAME || aws ecr create-repository --repository-name $TARGET_REPOSITORY_NAME --image-scanning-configuration scanOnPush=true
+    sleep 10
     aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
     # Tagging and pushing Docker images according to https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-pull-ecr-image.html
     docker tag $image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$TARGET_REPOSITORY_NAME:${image_tag}
