c413590e589f04d2c79b2ec9b83c38bc6db67a83
malachi-constant <lucascchanson@gmail.com>
2024-07-08 15:05:28 -0700

    chore: organize integ stack class

--- modules/storage/efs/integ/integ_efs.py
+++ modules/storage/efs/integ/integ_efs.py
@@ -12,15 +12,13 @@ import stack  # noqa: E402
 
 app = cdk.App()
 timestamp = datetime.datetime.now()
-deps_stack = cdk.Stack(app, "EFS Integ Tests Dependencies")
-vpc = ec2.Vpc(deps_stack, "EFS Integ VPC")
 
-integration.IntegTest(
-    app,
-    "Integration Tests EFS Module",
-    test_cases=[
-        stack.EFSFileStorage(
-            app,
+class EFSFileStorageWithDependencies(stack.EFSFileStorage):
+    def __init__(self, scope):
+        vpc = ec2.Vpc(scope, "EFS Integ VPC")
+
+        super().__init__(
+            scope,
             "efs",
             project_name="integ",
             deployment_name="testing",
@@ -29,6 +27,12 @@ integration.IntegTest(
             efs_removal_policy="DESTROY",
             stack_descrption=f"Integration Test: {timestamp.month}-{timestamp.day} {timestamp.hour}:{timestamp.minute}",
         )
+
+integration.IntegTest(
+    app,
+    "Integration Tests EFS Module",
+    test_cases=[
+        EFSFileStorageWithDependencies(app)
     ],
     diff_assets=True,
     stack_update_workflow=True,
