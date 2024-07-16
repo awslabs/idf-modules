7006aa0159fb06f7f54a1237dc5400fc2205100e
malachi-constant <lucascchanson@gmail.com>
2024-07-16 12:21:05 -0700

    chore: fix integration test to lookup dependencies before test, adds boto3 to requirements

--- modules/storage/efs/integ/integ_efs.py
+++ modules/storage/efs/integ/integ_efs.py
@@ -1,10 +1,13 @@
 import datetime
+import logging
+import os
 import sys
+from typing import Dict, List
 
 import aws_cdk as cdk
-import aws_cdk.aws_ec2 as ec2
 import aws_cdk.cloud_assembly_schema as cas
 import aws_cdk.integ_tests_alpha as integration
+import boto3
 
 sys.path.append("../")
 
@@ -12,28 +15,46 @@ import stack  # noqa: E402
 
 app = cdk.App()
 timestamp = datetime.datetime.now()
+logger = logging.getLogger(__name__)
 
-class EFSFileStorageWithDependencies(stack.EFSFileStorage):
-    def __init__(self, scope):
-        vpc = ec2.Vpc(scope, "EFS Integ VPC")
+def get_module_dependencies(resource_keys: List[str]) -> Dict[str, str]:
+    ssm = boto3.client("ssm", region_name="us-east-1")
+    dependencies = {}
+    try:
+        for key in resource_keys:
+            dependencies[key] = ssm.get_parameter(Name=f"/module-integration-tests/{key}")['Parameter']['Value']
+    except Exception as e:
+        print(f"issue getting dependencies: {e}")
+    return dependencies
 
-        super().__init__(
-            scope,
-            "efs",
-            project_name="integ",
-            deployment_name="testing",
-            module_name="efs",
-            vpc_id=vpc.vpc_id,
-            efs_removal_policy="DESTROY",
-            stack_descrption=f"Integration Test: {timestamp.month}-{timestamp.day} {timestamp.hour}:{timestamp.minute}",
-        )
+
+dependencies = get_module_dependencies(
+    # Add any required resource identifiers here
+    resource_keys=[
+        "vpc-id"
+    ]
+)
 
 integration.IntegTest(
     app,
     "Integration Tests EFS Module",
     test_cases=[
-        EFSFileStorageWithDependencies(app)
+        stack.EFSFileStorage(
+            app,
+            "efs",
+            project_name="integ",
+            deployment_name="testing",
+            module_name="efs",
+            vpc_id=dependencies["vpc-id"],
+            efs_removal_policy="DESTROY",
+            env=cdk.Environment(
+                account=os.getenv("CDK_DEFAULT_ACCOUNT"),
+                region="us-east-1"
+            )
+        )
     ],
+    regions=["us-east-1"],
+    enable_lookups=True,
     diff_assets=True,
     stack_update_workflow=True,
     cdk_command_options=cas.CdkCommands(
