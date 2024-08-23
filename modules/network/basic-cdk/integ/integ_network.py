5ae7b80aba9d543f2a08d7ac4bde576b0d64e7af
Ethan Bunce <ebunce@amazon.co.uk>
2024-08-23 09:54:10 +0000

    Remove local zones from integration tests

--- modules/network/basic-cdk/integ/integ_network.py
+++ modules/network/basic-cdk/integ/integ_network.py
@@ -1,11 +1,9 @@
 import datetime
-import os
 import sys
 
 import aws_cdk as cdk
 import aws_cdk.cloud_assembly_schema as cas
 import aws_cdk.integ_tests_alpha as integration
-import boto3
 
 sys.path.append("../")
 
@@ -13,10 +11,6 @@ import stack  # noqa: E402
 
 app = cdk.App()
 timestamp = datetime.datetime.now()
-
-ec2_client = boto3.client("ec2", region_name=os.getenv("AWS_REGION"))
-response = ec2_client.describe_availability_zones()
-availability_zones = [az["ZoneName"] for az in response["AvailabilityZones"]][:3]
 
 integration.IntegTest(
     app,
@@ -31,7 +25,7 @@ integration.IntegTest(
             vpc_cidr="10.0.0.0/16",
             cidr_mask=24,
             internet_accessible=True,
-            local_zones=availability_zones,
+            local_zones=[],
             stack_description=f"""
             Integration Test: {timestamp.month}-{timestamp.day} {timestamp.hour}:{timestamp.minute}
             """,
@@ -45,7 +39,7 @@ integration.IntegTest(
             vpc_cidr="10.0.0.0/16",
             cidr_mask=24,
             internet_accessible=False,
-            local_zones=availability_zones,
+            local_zones=[],
             stack_description=f"""
             Integration Test: {timestamp.month}-{timestamp.day} {timestamp.hour}:{timestamp.minute}
             """,
